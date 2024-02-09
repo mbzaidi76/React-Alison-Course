ff07c176b2e0f8ae65143a99545f8529b942af43
Mb Zaidi <mbzaidi76@gmail.com>
2024-02-09 16:09:10 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*function MyForm() {
+    const [textarea, setTextarea]= useState ([
+        <p key="paragraph">Ullamco tempor cillum deserunt laboris. Sunt sunt do ad id deserunt ea commodo esse incididunt deserunt quis quis ullamco. Ad sint dolor ea consectetur. Et id occaecat laboris dolor aliqua incididunt deserunt sit. Reprehenderit esse magna sunt nostrud ullamco ad. Minim irure aliqua excepteur sit cillum anim ea aliqua veniam occaecat aliqua. Mollit dolor minim amet reprehenderit nisi culpa nostrud sit non veniam excepteur voluptate dolor</p>,
+        "Wow this is amazing."
+    ]);
+
+    
+const handleChange= (event)=>{
+    setTextarea(event.target.value)
+
+}
+
+return(
+    <form>
+        <textarea value={textarea} onChange={handleChange}/>
+    </form>
+)
+}
+
+const root= ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyForm/>);
+*/
+function MyDropDownList() {
+    const [myCar, setMyCar]= useState("Audi");
+    const handleChange= (event)=>{
+    setMyCar(event.target.value)
+    }
+return(
+    <form>
+        <select value={myCar} onChange={handleChange}> 
+        <option value="Ford">Ford</option>
+        <option value="Mustang">Mustang</option>
+        <option value="Ferrari">Ferrari</option>
+        </select>
+    </form>
+)
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MyDropDownList/>);
